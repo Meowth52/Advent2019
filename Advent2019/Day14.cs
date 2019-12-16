54b15d0ff0828f385dbf35fd2fcaa08232d30370
Meowth52 <meowth@meowth.se>
2019-12-16 21:53:59 +0100

    Update Day14.cs

--- Advent2019/Day14.cs
+++ Advent2019/Day14.cs
@@ -10,10 +10,12 @@ namespace Advent2019
     public class Day14 : Day
     {
         Dictionary<string, Dictionary<string,int>> Reactions;
+        Dictionary<string, int> Materials;
         public Day14(string _input) : base(_input)
         {
             string[] Instructions = this.parseStringArray(_input);
             Reactions = new Dictionary<string, Dictionary<string, int>>();
+            Materials = new Dictionary<string, int>();
             foreach(string s in Instructions)
             {
                 MatchCollection Matches = Regex.Matches(s, @"(-?\d+) ([A-Z]+)");
@@ -23,27 +25,32 @@ namespace Advent2019
                     Row.Add(m.Groups[2].Value, Int32.Parse(m.Groups[1].Value));
                 }
                 Reactions.Add(Row.Last().Key, Row);
+                Materials.Add(Row.Last().Key, 0);
             }
         }
         public override Tuple<string, string> getResult()
         {
-            int Sum = ReCurse("FUEL");
+            int Sum = ReCurse("FUEL", 1);
 
             int Sum2 = 0;
             return Tuple.Create(Sum.ToString(), Sum2.ToString());
         }
-        public int ReCurse(string that)
+        public int ReCurse(string that, int wanted)
         {
-            int RetuenValue = 0;
+            int ReturnValue = 0;
             foreach(KeyValuePair<string,int> eh in Reactions[that])
             {
                 if (eh.Key == "ORE")
-                    RetuenValue += eh.Value;
+                    ReturnValue += GetCost(wanted - Materials[that], Reactions[that].Last().Value, eh.Value);
                 else if (eh.Key != that)
-                    RetuenValue += ReCurse(eh.Key) * eh.Value;
+                    ReturnValue += GetCost(wanted - Materials[that], Reactions[that].Last().Value, ReCurse(eh.Key, eh.Value));
             }
-            RetuenValue = (int)Math.Ceiling((double)RetuenValue / (double)Reactions[that].Last().Value);
-            return RetuenValue;
+            Materials[that] = (int)Math.Ceiling((double)(wanted - Materials[that]) / (double)Reactions[that].Last().Value) * Reactions[that].Last().Value;
+            return ReturnValue;
+        }
+        public int GetCost(int wanted, int provided, int cost)
+        {
+            return (int)Math.Ceiling((double)wanted/(double)provided)*cost;
         }
     }
     //public class Schmenum
