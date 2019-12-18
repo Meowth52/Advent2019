69aca5a8b805d538116d16f7ddb5cc3f0f69002a
Meowth52 <meowth@meowth.se>
2019-12-18 19:04:39 +0100

    Day 14 done

--- Advent2019/Day14.cs
+++ Advent2019/Day14.cs
@@ -9,18 +9,18 @@ namespace Advent2019
 {
     public class Day14 : Day
     {
-        Dictionary<string, Dictionary<string,int>> Reactions;
+        Dictionary<string, Dictionary<string, int>> Reactions;
         Dictionary<string, long> Materials;
         public Day14(string _input) : base(_input)
         {
             string[] Instructions = this.parseStringArray(_input);
             Reactions = new Dictionary<string, Dictionary<string, int>>();
             Materials = new Dictionary<string, long>();
-            foreach(string s in Instructions)
+            foreach (string s in Instructions)
             {
                 MatchCollection Matches = Regex.Matches(s, @"(-?\d+) ([A-Z]+)");
                 Dictionary<string, int> Row = new Dictionary<string, int>();
-                foreach(Match m in Matches)
+                foreach (Match m in Matches)
                 {
                     Row.Add(m.Groups[2].Value, Int32.Parse(m.Groups[1].Value));
                 }
@@ -31,27 +31,49 @@ namespace Advent2019
         public override Tuple<string, string> getResult()
         {
             long Sum = ReCurse("FUEL", 1);
+            long Target = 1000000000000;
+            long Fuel = Target;
+            long Ore = 0;
+            long LastGuess = 1;
+            long AnotherLastGuess;
+            long SmallestDiff = Target;
             long Sum2 = 0;
-            while (Sum2 != 1000000000000)
+            while (Fuel != LastGuess)
             {
-
+                Ore = ReCurse("FUEL", Fuel);
+                AnotherLastGuess = Fuel;
+                if (Ore <= Target)
+                {
+                    if (Target - Ore < SmallestDiff)
+                    {
+                        SmallestDiff = Target - Ore;
+                        Sum2 = Fuel;
+                    }
+                }
+                if (Ore < Target)
+                {
+                    Fuel += (long)Math.Ceiling(Math.Abs(Fuel - LastGuess) / (double)2);
+                }
+                else if (Ore > Target)
+                    Fuel -= (long)Math.Floor(Math.Abs(Fuel - LastGuess) / (double)2);
+                LastGuess = AnotherLastGuess;
             }
             return Tuple.Create(Sum.ToString(), Sum2.ToString());
         }
         public long ReCurse(string that, long wanted)
         {
             long ReturnValue = 0;
-            long RoundUp = ((long)Math.Ceiling(wanted / (float)Reactions[that].Last().Value) );
-            foreach (KeyValuePair<string,int> Next in Reactions[that])
+            long RoundUp = ((long)Math.Ceiling(wanted / (float)Reactions[that].Last().Value));
+            foreach (KeyValuePair<string, int> Next in Reactions[that])
             {
                 if (Next.Key == "ORE")
                     ReturnValue += RoundUp * Next.Value;
-                else if (Next.Key != that )
+                else if (Next.Key != that)
                 {
                     ReturnValue += ReCurse(Next.Key, RoundUp * Next.Value - Materials[Next.Key]);
                 }
             }
-            Materials[that] = RoundUp * Reactions[that].Last().Value - (wanted );
+            Materials[that] = RoundUp * Reactions[that].Last().Value - (wanted);
             return ReturnValue;
         }
     }
